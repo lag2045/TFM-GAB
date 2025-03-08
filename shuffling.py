0c106b5e4f67f8f5dcabde4f6a40cbbae0cf2d81
lag2045 <cern30@gmail.com>
2025-03-08 11:15:33 -0600

    Codigo_Shuffling.py

--- shuffling.py
+++ shuffling.py
@@ -1,8 +1,1 @@
-import numpy as np
-from scipy.spatial import cKDTree
-import matplotlib.pyplot as plt
-import os
-
-# Cargar el catálogo de datos desde cat_z00.npy
-file_path = "/Users/hakeem/Desktop/cat_z00.npy"
 
